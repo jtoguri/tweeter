46db22544c9814acb5b9b2fa7435a5ef58c5825f
Joshua Toguri <joshua.toguri@gmail.com>
2022-03-04 08:41:29 -0500

    implemented get and post requests to view and change the stored tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,53 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$( document ).ready(function() {
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <img src="${tweet.user.avatars}" alt="pfp">
+        <span>${tweet.user.name}</span>
+        <span>${tweet.user.handle}</span>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <span>${tweet.created_at} days ago</span>
+        <ul>
+          <li><i class="fa-solid fa-flag"></i></li>
+          <li><i class="fa-solid fa-retweet"></i></li>
+          <li><i class="fa-solid fa-heart"></i></li>
+        </ul>
+      </footer>
+    </article>`);
+    return $tweet;
+  }
+
+  const loadTweets = function() {
+    $.get("/tweets", function( data ) {
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
+
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+       
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append($tweet);
+    }
+  }
+
+  // NOTE* ask ian about how express .static(public) knows to access the index.html see link below for what seems to be the explanantion, specifically check out the connect docs
+  // https://stackoverflow.com/questions/10857393/how-to-make-label-visible-invisible
+
+  $( "form" ).submit(function( e ) {
+    e.preventDefault();
+    const newTweet = $( this ).serialize();
+    $.post("/tweets", newTweet);
+  })
+});
